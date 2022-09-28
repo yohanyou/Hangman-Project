5981cb8409cfc90f303b6d24e2261ab67b74c6b6
Yohan You <yohan.you@ug.bilkent.edu.tr>
2022-09-28 15:26:04 +0300

    Framework for Hangman class + properties

--- Hangman.java
+++ Hangman.java
@@ -1,4 +1,22 @@
 public class Hangman{
-
     
+
+
+    Hangman(){
+
+        
+    }    
+
+    Hangman (StringBuffer secretWord){
+        //used to remove/append to usedLetters (when it gets removed, it becomes "used")
+        StringBuffer allLetters = new StringBuffer(26);
+        allLetters.append("abcdefghijklmnopqrstuvwxyz");
+
+        //used to record the used letters (to see if the user inputs the same characters)
+        StringBuffer usedLetters = new StringBuffer(26);
+
+        int numberOfIncorrectTries = 0;
+        int maxAllowedIncorrectTries = 6;
+
+    }
 }
